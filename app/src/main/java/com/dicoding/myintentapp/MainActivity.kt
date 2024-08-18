1f738ae9f0408ececad47bce6c9f4bf6181b0bec
Kyushin45 <mohammaddivaatalarik@gmail.com>
2024-08-18 18:56:27 +0700

    first commit

---
+++ app/src/main/java/com/dicoding/myintentapp/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.dicoding.myintentapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btnmoveActivity: Button = findViewById(R.id.btn_move_activity)
+        btnmoveActivity.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?){
+        when(v?.id){
+            R.id.btn_move_activity -> {
+                val moveIntent = Intent(this@MainActivity, MoveActivity::class.java)
+                startActivity(moveIntent)
+            }
+
+        }
+    }
+}
